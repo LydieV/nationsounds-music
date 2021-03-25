2b9c791577b3f3619693bf560eecd0b9d2bf288d
kenged <75262521+kenged@users.noreply.github.com>
2021-03-25 09:27:12 +0100

    FIrst commit Menu bar

--- front/src/Header.js
+++ front/src/Header.js
@@ -3,9 +3,20 @@ import Menu from './Menu';
 
 export default function Header()  {
     return (
-        <div className={"header"}>
-            <Menu></Menu>
-        </div>
+        <header class="header">
+        <a href="./" class="logo"><img src="https://svgshare.com/i/VPB.svg" alt="logo"/></a>
+        <a href="./Login" class="login"><img src="https://svgshare.com/i/VNn.svg" alt="login"/></a>
+
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        
+
+        <ul class="menu">
+         <Menu></Menu>
+        </ul>
+      </header>
     );
 
 }
+
+
