5af941d42cd820a1720cb711dbd1c2e5dd93454e
Télio Valencia <telioval@gmal.com>
2021-03-25 18:59:38 +0100

    Première partie Info
    tentatives en back de créer l'entité Info, mais impossibilité de récupérer les champs "titre" "description" et "type"
    Création de deux fichiers distinc FAQ.js et Info.js pour eviter les problème d'appel sur la page InfoFaq.js

--- front/src/InfosFAQ.js
+++ front/src/InfosFAQ.js
@@ -3,40 +3,37 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "./Header";
 import Tabs from "./Tabs";
+import Faq from "./Faq";
+import Info from "./Info";
 
 const InfosFAQ = () => {
-    let [infosfaqDatas, setInfosFAQDatas] = useState([])
+    let [infosfaqDatas, setInfosFAQDatas] = useState([]);
     useEffect(() => {
         axios
             .get('https://localhost:8000/api/faqs')
             .then(datas => {
                 setInfosFAQDatas(datas.data["hydra:member"])
             })
-    }, [])
+    }, []);
 
 
     return (
         <div>
             <Header></Header>
-            <p className={"faq"}> Liste faq 2 </p>
-   
+            <p className={"faq"}> c'est pour le margin </p>
+
             {/*JSON.stringify(infosfaqDatas)*/}
             <div className={"blockSwitchProgramme"}>
-            
+
+
 
             <Tabs>
                 <div title="Foire aux Questions" > 
-                {infosfaqDatas.map(infosfaqData =>
-                    <div className={"card-faq"}>
-                        <h3>{infosfaqData.question}</h3>
-                        <p>{infosfaqData.reponse}</p> 
-                    </div>
-                )}
+                    <Faq></Faq>
                 </div>
 
                 <div title="Informations">
-                    <p>Informations générales</p>
-                    <p>Affichage des notifications</p>
+                    <Info></Info>
                 </div>
             </Tabs>
             
