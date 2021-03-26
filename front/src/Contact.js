a0e4888eca4e8cb19dea98576413d4725071362c
Télio Valencia <telioval@gmal.com>
2021-03-26 20:08:41 +0100

    quelques modifs de front statiques
    Voici quelques modifs qui donneront un aspect visuel à certaines page, ça sera toujours ça de fait

--- front/src/Contact.js
+++ front/src/Contact.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Header from "./Header";
 
 export default function Contact(){
     return(
         <div>
-            <p> Page Contact </p>
+            <Header></Header>
+            <p className={"faq"}> c'est pour le margin </p>
+            <div className={"contact-form"}>
+                <h1>Contactez-nous ! </h1>
+                <input type="email" name="email" placeholder="Votre adresse mail"></input>
+                <input type="text" name="sujet" placeholder="Sujet de votre message"></input>
+                <textarea type="textarea" name="message" placeholder="Votre message"></textarea>
+                <button>Envoyer</button>
+            </div>
+            <hr></hr>
+            <div className={"contact-rs"}>
+                <p>Vous avez un problème ?</p>
+                <p>Contactez-nous sur les réseaux sociaux :</p>
+            </div>
+            <hr></hr>
+            <div className={"contact-mail"}>
+                <p>Vous souhaitez nous contacter par mail ?</p>
+                <a href="#">contact@nation-sound.fr</a>
+            </div>
         </div>
 
     );
