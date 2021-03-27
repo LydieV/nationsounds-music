0ab2b2b197911df8d2ae0b6906fbdc44eb6fb5e7
Télio Valencia <telioval@gmal.com>
2021-03-27 13:44:57 +0100

    un peu de front pour le profil

--- front/src/Profil.js
+++ front/src/Profil.js
@@ -1,9 +1,39 @@
 import React from "react";
+import Header from "./Header";
+import Tabs from "./Tabs";
 
 export default function Profil(){
     return(
         <div>
-            <p> Page Profil </p>
+            <div>
+            <Header></Header>
+            <p className={"faq"}> c'est pour le margin </p>
+
+            {/*JSON.stringify(infosfaqDatas)*/}
+            <div className={"blockSwitchProgramme"}>
+
+
+
+            <Tabs>
+                <div title="Profil" > 
+                    <div className={"profil-info"}>
+                        <button>Modifier les informations</button>
+                        <input type="text" name="nom" placeholder="Votre nom"></input>
+                        <input type="text" name="prenom" placeholder="Votre prénom"></input>
+                        <input type="email" name="email" placeholder="Votre adresse mail"></input>
+                        <input type="password" name="password" placeholder="•••••••••••"></input>
+                    </div>
+                </div>
+
+                <div title="Billet">
+                    <div className={"billet-info"}>
+                        <button>Voir mon billet</button>
+                    </div>
+                </div>
+            </Tabs>
+            
+            </div>
+        </div>
         </div>
 
     );
