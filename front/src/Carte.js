0a1aa18e1cd8ac229064dfc3802e6c95c2602dfb
kenged <75262521+kenged@users.noreply.github.com>
2021-03-28 20:00:54 +0200

    Page carte inter.
    Manque les notification j'attends télio

---
+++ front/src/Carte.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Carte()  {
+    return (
+        <div>
+            <iframe title ="umap" className="map" src="//umap.openstreetmap.fr/fr/map/nation-sound_554628?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false" ></iframe>
+        </div>
+    )
+}
+
