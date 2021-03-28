d133209f19273606f66463259f41189978b8f7bd
LydieV <49896819+LydieV@users.noreply.github.com>
2021-03-28 11:10:16 +0200

    Modifications ClassName.

--- front/src/Header.js
+++ front/src/Header.js
@@ -4,26 +4,26 @@ import Menu from './Menu';
 export default function Header()  {
     return (
         <div>
-        <div class="header-menu">
-        <a href="./" class="logo"><img src="https://svgshare.com/i/VPB.svg" alt="logo"/></a>
-        <a href="./Login" class="login"><img src="https://svgshare.com/i/VNn.svg" alt="login"/></a>
-      </div>
+          <div className={"header-menu"}>
+            <a href="./" className={"logo"}><img src="https://svgshare.com/i/VPB.svg" alt="logo"/></a>
+            <a href="./Login" className={"login"}><img src="https://svgshare.com/i/VNn.svg" alt="login"/></a>
+          </div>
 
-        <div id="nav-container">
-        <div class="bg"></div>
-        <div class="button" tabindex="0">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </div>
-        <div id="nav-content" tabindex="0">
-          <Menu></Menu>
-        </div>
+          <div id="nav-container">
+            <div className={"bg"}></div>
+
+            <div className={"button"} tabIndex="0">
+              <span className={"icon-bar"}></span>
+              <span className={"icon-bar"}></span>
+              <span className={"icon-bar"}></span>
+            </div>
+
+            <div id="nav-content" tabindex="0">
+              <Menu></Menu>
+            </div>
+
+          </div>
       </div>
-      </div>
-    
-    
-      
     );
 
 }
