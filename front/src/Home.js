485ddc02e3d722717c4871709ac565ece41141f8
flyforday <leo.lemaire@outlook.com>
2021-03-29 22:11:44 +0200

    Integration Cookie
    faire cette commande pour ajouter :
    
    npm install react-cookie-consent

--- front/src/Home.js
+++ front/src/Home.js
@@ -6,6 +6,7 @@ import {useCookies} from 'react-cookie';
 import {Redirect} from 'react-router-dom';
 import Footer from "./Footer";
 import Carte from "./Carte";
+import CookieConsent from "react-cookie-consent";
 
 
 function Home() {
@@ -27,7 +28,45 @@ function Home() {
 
                 <Carte></Carte>
                 </div>
-
+                <CookieConsent
+                    onAccept={() => {
+                        alert("C'est ok tu as accepté les cookies");
+                    }}
+                    enableDeclineButton
+                    onDecline={() => {
+                        alert("Noooon les cookiiies !");
+                    }}
+                    location="bottom"
+                    buttonText="Accepter"
+                    declineButtonText="Décliner"
+                    cookieName="myAwesomeCookieName2"
+                    style={{ background: "white", 
+                    color :"#34383F",
+                    borderRadius: "8px",
+                    margin : "30px",
+                    boxShadow: "0px 4px 25px rgba(0, 0, 0, 0.1)",
+                    width : "none"
+                    }}
+                    buttonStyle={{ color: "white", 
+                    fontSize: "14px",
+                    boxShadow: "inset -3.63243px -5.38531px 6.49585px #6F00BD, inset 3.63243px 5.38531px 6.49585px #B500FF",
+                    filter: "drop-shadow(10.0282px 14.8674px 35.8667px rgba(30, 0, 51, 0.3205))",
+                    background: "linear-gradient(146deg, #AD00FF 0%, #7700CA 72.03%)",
+                    borderRadius: "8px",
+                      }}
+                    declineButtonStyle={{
+                        color: "#9600FF", 
+                        fontSize: "14px",
+                        border: "2px solid #9600FF",
+                        boxSizing: "border-box",
+                        borderRadius: "8px",
+                        background: "white"
+                    }}
+                    expires={150}
+                    >
+                    Ce site utilise des cookies pour personnaliser votre navigation.<br></br>
+                    Acceptez-vous l'utilisation des cookies ?
+                </CookieConsent>
                 <Footer></Footer>
             </div>
         )
@@ -44,6 +83,45 @@ function Home() {
                 <div className={"map-home"}>
                 <Carte></Carte>
                 </div>
+                <CookieConsent
+                    onAccept={() => {
+                        alert("C'est ok tu as accepté les cookies");
+                    }}
+                    enableDeclineButton
+                    onDecline={() => {
+                        alert("Noooon les cookiiies !");
+                    }}
+                    location="bottom"
+                    buttonText="Accepter"
+                    declineButtonText="Décliner"
+                    cookieName="myAwesomeCookieName2"
+                    style={{ background: "white", 
+                    color :"#34383F",
+                    borderRadius: "8px",
+                    margin : "30px",
+                    boxShadow: "0px 4px 25px rgba(0, 0, 0, 0.1)",
+                    width : "none"
+                    }}
+                    buttonStyle={{ color: "white", 
+                    fontSize: "14px",
+                    boxShadow: "inset -3.63243px -5.38531px 6.49585px #6F00BD, inset 3.63243px 5.38531px 6.49585px #B500FF",
+                    filter: "drop-shadow(10.0282px 14.8674px 35.8667px rgba(30, 0, 51, 0.3205))",
+                    background: "linear-gradient(146deg, #AD00FF 0%, #7700CA 72.03%)",
+                    borderRadius: "8px",
+                      }}
+                    declineButtonStyle={{
+                        color: "#9600FF", 
+                        fontSize: "14px",
+                        border: "2px solid #9600FF",
+                        boxSizing: "border-box",
+                        borderRadius: "8px",
+                        background: "white"
+                    }}
+                    expires={150}
+                    >
+                    Ce site utilise des cookies pour personnaliser votre navigation.<br></br>
+                    Acceptez-vous l'utilisation des cookies ?
+                </CookieConsent>
                 <Footer></Footer>
                 
             </div>
